162a36e1e7b5a3b0aec35cf527734d4634d1903c
Meghan - The Tidy Trekker <meghansa@buffalo.edu>
2022-07-06 23:33:47 -0400

    Final testing and annotation.

--- A_Fertility_Journey/app.R
+++ A_Fertility_Journey/app.R
@@ -1,19 +1,24 @@
-library(shiny)
-library(bslib)
+# A fertility Journey <3
 
-# Define UI for application that draws a histogram
+# Library Load-in====
+library(shiny) # For making the app
+
+
+# Defining a simple UI for displaying an image behind a pop-up dialog box===
 ui <- fluidPage(id = 'test',
                 tags$style('#test {
-                             background-color: #000000;
-              }'),
+                             background-color: #000000;}'),
               
+    # Modal dialog boxes gives a pop-up dialog box on the shiny app. 
+    # Style settings can be adjusted for individual needs. 
+    # These options completely hides the visual UNLESS the user clicks the green button
     
     modalDialog(title = "Content Warning",
                 tags$head(tags$style(".modal-dialog{ width:50vw}")),
                 tags$head(tags$style(".modal-body{ min-height:100%}")),
                 tags$head(tags$style(".modal-backdrop{ opacity:1 !important;}")),
                 span("The visual on this page mentions topics that may be sensitve or triggering for some like miscarriages, infertility, and pregnancy. If you do not wish to view this material please close this window or press the 'Back' button on your browser to exit."),
-                footer = HTML('<button style="position: relative; center: 0%; background-color: #f44336;" onclick="self.close()" style = >DON\'T Show the Visual (Close)</button>
+                footer = HTML('<button style="position: relative; center: 0%; background-color: #f44336;" onclick="top.close()" style = >DON\'T Show the Visual (Close)</button>
                                <button style="position: relative; center: 0%; background-color: #4CAF50;" type="button"  data-dismiss="modal" data-bs-dismiss="modal">Show the Visual (Continue)</button>
                               '),
                 size = c("s"),
@@ -21,18 +26,18 @@ ui <- fluidPage(id = 'test',
                 fade = TRUE),
     
     
+    # Simply placing an externally made plot into the shiny UI====
     
-    
-    # Show a plot of the generated distribution
-
-        HTML("<center>"),     img(src = "background.png", width = "660px", height = "1000px" ),  HTML("<center>")
+    HTML("<center>"), 
+    img(src = "background.png", width = "660px", height = "1000px" ),  
+    HTML("<center>")
     )
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic required====
 server <- function(input, output) {
-
+  # All elements are handled manually in the UI. Server still needs to be defined to run though.
 }
 
 # Run the application 
